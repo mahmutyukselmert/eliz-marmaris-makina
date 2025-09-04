9047fbff599c9a185879409b5c34a36e3d9b1129
Mahmut Yüksel Mert <mahmutyukselmert@gmail.com>
2025-09-04 11:37:43 +0300

    shell-sectionu yapmaya başladım

--- public/index.php
+++ public/index.php
@@ -17,7 +17,7 @@
 
         <section class="references-section">
             <div class="container">
-                <div class="row gap-5">
+                <div class="row align-items-center justify-content-between">
                     <div class="col-6 col-md-4 col-lg-2">
                         <a href="#" class="references-logos">
                             <img src="./assets/images/logolar-1.webp" alt="" class="img-fluid">
@@ -42,6 +42,58 @@
                         <a href="#" class="references-logos">
                             <img src="./assets/images/logolar-5.webp" alt="" class="img-fluid">
                         </a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="homepage-two-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-5 offset-lg-7">
+                        <div class="row white-area">
+                            <div class="col-lg-6">
+                                <h2>Yağlar</h2>
+                                <div class="content">
+                                    <p>Shell’in yağlar ve kimyasallar portföyü, otomotivden endüstriye, denizcilikten havacılığa kadar çok geniş bir sektöre hizmet eder. Shell bu alanda yüksek teknolojiye sahip madeni yağlar, gresler, soğutucular, hidrolik sıvılar ve özel kimyasallar sunar.</p>
+                                </div>
+                                <a href="#" class="btn">ÜRÜNLER</a>
+                            </div>
+                            <div class="col-lg-6">
+                                <img src="./assets/images/yaglar-sag-gorsel.webp" class="img-fluid" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="shell-section">
+            <div class="container">
+                <div class="row first-shell-area">
+                    <div class="col-lg-6 left-image-area">
+                        <img src="./assets/images/shell-panolin.webp" alt="" class="img-full">
+                    </div>
+                    <div class="col-lg-6 right-red-area px-5">
+                        <div class="heading">
+                            <h2>Shell Panolin</h2>
+                            <img src="./assets/images/shell-panolin-logo.webp" class="img-fluid">
+                        </div>
+                        <div class="content">
+                            <p>Shell Panolin, Shell’in sürdürülebilirlik odaklı yağlayıcı çözümleri kapsamında sunduğu, çevreye duyarlı biyolojik olarak kolay parçalanabilen (biodegradable) hidrolik ve şanzıman yağları serisidir. Bu yağlar özellikle çevresel hassasiyetin yüksek olduğu alanlarda —örneğin su kaynakları çevresi, tarım alanları, inşaat ve ormancılık sektörlerinde— kullanılmak üzere geliştirilmiştir.</p>
+                        </div>
+                        <a href="" class="btn btn-red">
+                            ÜRÜNLER
+                        </a>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-6">
+
+                    </div>
+                    <div class="col-lg-6">
+
                     </div>
                 </div>
             </div>
